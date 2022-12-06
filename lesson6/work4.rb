d994e0951669dc01917acc04dc885778c3a4cf95
Khazirov804 <90681581+Khazirov804@users.noreply.github.com>
2022-12-06 18:49:07 +0300

    Update work4.rb

--- lesson6/work4.rb
+++ lesson6/work4.rb
@@ -1,1 +1,13 @@
-100.times {|i| puts "i = #{i}" }
+print "Какую сумму будем откладывать в месяц: "
+sum = gets.to_f
+
+print "Сколько месяцев будем откладывать: "
+month = gets.to_i
+
+s = 0
+
+1.upto(month) do |mm|
+	s = s + sum
+	puts "Накопления за #{mm} месяц: #{s} "
+end
+puts "За #{month} месяцев накоплено: #{s}"
